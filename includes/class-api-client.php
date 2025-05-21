443747d467675d6f0113dd4d73abfab93890b581
oalvarezoliveira <oalvarezoliveira@gmail.com>
2025-05-21 14:39:41 +0200

    log fix

--- includes/class-api-client.php
+++ includes/class-api-client.php
@@ -44,4 +44,39 @@ class Engel_API_Client {
 	public function fetch_all_products() {
 		return $this->request('/products');
 	}
+
+	public function login($username, $password) {
+		$url = $this->base_url . '/login';
+
+		$response = wp_remote_post($url, [
+			'body' => json_encode([
+				'username' => $username,
+				'password' => $password,
+			]),
+			'headers' => [
+				'Content-Type'  => 'application/json',
+				'Accept'        => 'application/json'
+			],
+			'timeout' => 15,
+		]);
+
+		if (is_wp_error($response)) {
+			return $response;
+		}
+
+		$code = wp_remote_retrieve_response_code($response);
+		$body = wp_remote_retrieve_body($response);
+
+		if ($code !== 200) {
+			return new WP_Error('login_error', "Login fallido: $code");
+		}
+
+		$data = json_decode($body, true);
+
+		if (json_last_error() !== JSON_ERROR_NONE) {
+			return new WP_Error('json_error', 'Error al decodificar JSON.');
+		}
+
+		return isset($data['token']) ? $data['token'] : new WP_Error('token_error', 'Token no encontrado.');
+	}
 }
