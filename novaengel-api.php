d51c60079d69c52ad756cb13b0848fe2ffac886b
oalvarezoliveira <oalvarezoliveira@gmail.com>
2025-05-22 11:28:41 +0200

    versión 3.3.2

--- novaengel-api.php
+++ novaengel-api.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Engel Sync
 Description: Sincroniza productos desde la API de NovaEngel automáticamente mediante cron y guarda logs de sincronización.
-Version: 1.3.1
+Version: 1.3.2
 Author: ChatGPT
 */
 
